2b68277eabde2e0408b983db6ef0b0fea0a0cd74
Viktor <zazuliaviktor4@gmail.com>
2025-12-01 15:25:26 +0200

    Lab 4 added

---
+++ lb4/task5.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const sum = (...args) => {
+  return args.reduce((acc, val) => acc + val, 0);
+};
+
+console.log(sum(1, 2, 3));        
+console.log(sum(0));              
+console.log(sum());               
+console.log(sum(1, -1, 1));       
+console.log(sum(10, -1, -1, -1)); 
---
+++ lb4/task7.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const ages = (persons) => {
+  const result = {};
+
+  for (const name in persons) {
+    const person = persons[name];
+    const lifespan = person.died - person.born;
+    result[name] = lifespan;
+  }
+
+  return result;
+};
+
+const persons = {
+  lenin: { born: 1870, died: 1924 },
+  mao: { born: 1893, died: 1976 },
+  gandhi: { born: 1869, died: 1948 },
+  hirohito: { born: 1901, died: 1989 },
+};
+
+console.log(ages(persons));
